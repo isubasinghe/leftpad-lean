092e362e287ee58c99bd7b592c91a0d63e663406
isubasinghe <isubasinghe@student.unimelb.edu.au>
2023-09-12 21:53:04 +1000

    proof

--- Leftpad.lean
+++ Leftpad.lean
@@ -1,4 +1,5 @@
 import Aesop
+
 def leftpad (l: List α) (a: α) (n: Nat) : List α := (List.replicate n a) ++ l
 
 
@@ -12,13 +13,35 @@ def drop (l: List α) (n: Nat): Option (List α) :=
 
 #eval drop [1, 2] 1
 
-@[simp]
-theorem drop_leftpad_eq_orig (l: List α) (a: α) (n: Nat): drop (leftpad l a n) n = l := by sorry
 
+theorem drop_leftpad_eq_orig (l: List α) (a: α) (n: Nat): drop (leftpad l a n) n = l := by
+  induction n with
+  | zero => 
+    simp
+    rw [leftpad]
+    simp
+    unfold drop
+    simp
+  | succ z iz => 
+    rw [<-Nat.add_one]
+    rw [<-iz]
+    rfl
 
-theorem nil_append (l: List Nat): l ++ [] ++ l  ++ [2]= l ++ l ++ [2] := by 
+theorem take_repl_eq_repl (a: α) (n: Nat): (List.replicate n a).take n = List.replicate n a := by
+  induction n with 
+  | zero => 
+    simp
+  | succ z iz => 
+    simp
+    rw [iz] 
+
+theorem take_leftpad_eq_padded (l: List α) (a: α) (n: Nat):(leftpad l a n).take n = List.replicate n a := by
+induction n with
+| zero =>
   simp
-
-
-
+| succ z iz => 
+  rw [<-Nat.add_one]
+  simp
+  rw [<-leftpad]
+  rw [iz]
 
