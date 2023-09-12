aa3e3c825b660380c3ae11e826b35e06e04ba59d
isubasinghe <isubasinghe@student.unimelb.edu.au>
2023-09-12 22:07:49 +1000

    drop Aesop

--- Leftpad.lean
+++ Leftpad.lean
@@ -1,5 +1,3 @@
-import Aesop
-
 def leftpad (l: List α) (a: α) (n: Nat) : List α := (List.replicate n a) ++ l
 
 
@@ -10,9 +8,6 @@ def drop (l: List α) (n: Nat): Option (List α) :=
     match l with 
     | [] => none 
     | _::xs => drop xs (n-1)
-
-#eval drop [1, 2] 1
-
 
 theorem drop_leftpad_eq_orig (l: List α) (a: α) (n: Nat): drop (leftpad l a n) n = l := by
   induction n with
@@ -31,17 +26,21 @@ theorem take_repl_eq_repl (a: α) (n: Nat): (List.replicate n a).take n = List.r
   induction n with 
   | zero => 
     simp
+    rw [List.take]
   | succ z iz => 
-    simp
-    rw [iz] 
+    rw [<-Nat.add_one]
+    simp [List.take]
+    rw [iz]
+
 
 theorem take_leftpad_eq_padded (l: List α) (a: α) (n: Nat):(leftpad l a n).take n = List.replicate n a := by
 induction n with
 | zero =>
   simp
+  rw [List.take] 
 | succ z iz => 
   rw [<-Nat.add_one]
-  simp
+  simp [List.take]
   rw [<-leftpad]
   rw [iz]
 
